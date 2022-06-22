ca44e14362cf183fb87e61d41ba6c7a8981b6780
Tyler Egashira <wildwoodwaltz@gmail.com>
2022-06-22 14:43:42 -0700

    added bot commands with prefixes

--- cividexter/cividexterbot.py
+++ cividexter/cividexterbot.py
@@ -1,43 +1,61 @@
+from unicodedata import name
 import discord, os, datetime
+from discord.ext import commands
 from dotenv import load_dotenv
+from helper import Helper
 
 
+bot = commands.Bot(command_prefix='$')
+helper = Helper()
 
 load_dotenv()
 
-client = discord.Client()
 token = os.getenv('TOKEN')
 
-def get_info():
+  
 
-   
-
-@client.event
+@bot.event
 async def on_ready():
-    string = f'Bot has initialized on {datetime.date.today()}'
-    print(string.format(client))
+    # string = f'Bot has initialized on {datetime.date.today()}'
+    get_fact = helper.request_get()
+    string = f"On {get_fact['date']}: {get_fact['fact']}"
+    print(string.format(bot))
 
 
-
-
-
-@client.event
+@bot.event
 async def on_message(message):
-    print (message)
+    '''
+    Overides message input and catches text for command operations
+    '''
     username = str(message.author).split("#")[0]
     channel = str(message.channel.name)
     user_message = str(message.content)
 
     print(f'Chatter on {channel} > {username} said: {user_message}')
 
-    if message.author == client.user:
+    if message.author == bot.user:
         return
- 
-    if channel == "cividex-development":
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
-            return
-        elif user_message.lower() == "bye":
-            await message.channel.send(f'Bye {username}')
 
-client.run(token)
+    await bot.process_commands(message)
+    
+
+@bot.command(name='date')
+async def date(ctx):
+    '''
+    Bot command retrieves dat with $date
+    '''
+    date_ = f'{datetime.date.today()}'
+    await ctx.send(date_)
+
+@bot.command(name ='fact')
+async def fact(ctx):
+    '''
+    Gets and displays random fact
+    '''
+    get_fact = helper.request_get()
+    string = f"On {get_fact['date']}: {get_fact['fact']}"
+    await ctx.send(string)
+
+
+if __name__ == "__main__":
+    bot.run(token)
